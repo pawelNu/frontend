39ceba99b98b3ec16db73389c9135237a03fb24a
pawelNu <pnurzynski17@gmail.com>
2024-08-25 09:00:57 +0200

    js progress 2024-08-25

--- javascript/25-regular-expressions/main.js
+++ javascript/25-regular-expressions/main.js
@@ -1,1 +1,19 @@
 // Regular expressions
+// https://regexr.com/
+
+document.getElementById("phoneNum");
+addEventListener("input", (e) => {
+    const regex = /^\(?(d{3})\)?[-. ]?(\d{3})[-. ]?(\d{4})$/g;
+    const input = document.getElementById("phoneNum");
+    const format = document.querySelector(".phoneFormat");
+    const phone = input.value;
+    const found = regex.test(phone);
+
+    if (!found && phone.length) {
+        input.classList.add("invalid");
+        format.classList.add("block");
+    } else {
+        input.classList.remove("invalid")
+        input.classList.remove("block")
+    }
+});
