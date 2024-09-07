bbdb3bc6a7c069e8f721be90291c20b0445bb426
pawelNu <pnurzynski17@gmail.com>
2024-09-07 18:23:52 +0200

    react js progress 2024 09 06

---
+++ 02-react-js/react-js-tutorial/src/components/11-useEffectHook/LineItem.jsx
@@ -0,0 +1,25 @@
+import { Trash2 } from "react-feather";
+
+const LineItem = ({ item, handleCheck, handleDelete }) => {
+    return (
+        <li className="item">
+            <input
+                type="checkbox"
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked}
+            />
+            <label
+                style={(item.checked) ? { textDecoration: 'line-through' } : null}
+                onDoubleClick={() => handleCheck(item.id)}
+            >{item.item}</label>
+            <Trash2
+                onClick={() => handleDelete(item.id)}
+                role="button"
+                tabIndex="0"
+                aria-label={`Delete ${item.item}`}
+            />
+        </li>
+    )
+}
+
+export default LineItem
